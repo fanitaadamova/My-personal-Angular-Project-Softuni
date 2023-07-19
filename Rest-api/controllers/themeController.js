eb8ef2f4598fd9cfccf79d6ae7679aa9dc9cc2d4
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-07-19 20:35:35 +0300

    My first commit
    Created initial design for pages, header, footer, some navigations, favicon

---
+++ Rest-api/controllers/themeController.js
@@ -0,0 +1,66 @@
+const { themeModel } = require('../models');
+const { newPost } = require('./postController')
+
+function getThemes(req, res, next) {
+    themeModel.find()
+        .populate('userId')
+        .then(themes => res.json(themes))
+        .catch(next);
+}
+
+function getLatestsThemes(req, res, next) {
+    const limit = Number(req.query.limit) || 0;
+
+    themeModel.find()
+        .sort({ created_at: -1 })
+        .limit(limit)
+        .populate('userId')
+        .then(themes => {
+            res.status(200).json(themes)
+        })
+        .catch(next);
+}
+
+function getTheme(req, res, next) {
+    const { themeId } = req.params;
+
+    themeModel.findById(themeId)
+        .populate({
+            path : 'posts',
+            populate : {
+              path : 'userId'
+            }
+          })
+        .then(theme => res.json(theme))
+        .catch(next);
+}
+
+function createTheme(req, res, next) {
+    const { themeName, postText } = req.body;
+    const { _id: userId } = req.user;
+
+    themeModel.create({ themeName, userId, subscribers: [userId] })
+        .then(theme => {
+            newPost(postText, userId, theme._id)
+                .then(([_, updatedTheme]) => res.status(200).json(updatedTheme))
+        })
+        .catch(next);
+}
+
+function subscribe(req, res, next) {
+    const themeId = req.params.themeId;
+    const { _id: userId } = req.user;
+    themeModel.findByIdAndUpdate({ _id: themeId }, { $addToSet: { subscribers: userId } }, { new: true })
+        .then(updatedTheme => {
+            res.status(200).json(updatedTheme)
+        })
+        .catch(next);
+}
+
+module.exports = {
+    getThemes,
+    getLatestsThemes,
+    createTheme,
+    getTheme,
+    subscribe,
+}
