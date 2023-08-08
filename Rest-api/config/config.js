b9d16ff9bf18919fb522fd66e7c5109b7206705d
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-08-08 20:26:44 +0300

    optimize mongo db url

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -3,13 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        //dbURL: 'mongodb://127.0.0.1:27017/androidForum',
-        dbURL: 'mongodb+srv://qwerty789456123:lL7SQXJNhyd7hx7c@cluster0.etqghoy.mongodb.net/?retryWrites=true&w=majority',
+        dbURL: 'mongodb://127.0.0.1:27017/androidForum',   
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
         port: process.env.PORT || 3000,
-        dbURL: process.env.DB_URL_CREDENTIALS,
+        dbURL: `mongodb+srv://qwerty789456123:${process.env.DB_URL_CREDENTIALS}@cluster0.etqghoy.mongodb.net/?retryWrites=true&w=majority`,
         origin: []
     }
 };
