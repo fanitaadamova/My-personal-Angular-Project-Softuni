4f0c1ab3e175987460e6c7221712d595569f996d
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-08-08 20:33:19 +0300

    update environment url

--- android-forum/src/environments/environment.development.ts
+++ android-forum/src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    apiUrl: 'http://localhost:3000/api'
+   // apiUrl: 'http://localhost:3000/api'
+   apiUrl: `https://android-forum-server.onrender.com/api`,
+   production: true,
 };
