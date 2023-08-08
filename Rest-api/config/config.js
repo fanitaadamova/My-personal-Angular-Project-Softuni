ef850d12d47ed72f0366316d6ecb7a5b8f00be3e
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-08-08 21:02:56 +0300

    restored dbURL

--- Rest-api/config/config.js
+++ Rest-api/config/config.js
@@ -3,12 +3,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbURL: 'mongodb://127.0.0.1:27017/androidForum',   
+        dbURL: 'mongodb://127.0.0.1:27017/androidForum',
         origin: ['http://localhost:5555', 'http://localhost:4200']
     },
     production: {
         port: process.env.PORT || 3000,
-        dbURL: `mongodb+srv://qwerty789456123:${process.env.DB_URL_CREDENTIALS}@cluster0.etqghoy.mongodb.net/?retryWrites=true&w=majority`,
+        dbURL: process.env.DB_URL_CREDENTIALS,
         origin: []
     }
 };
