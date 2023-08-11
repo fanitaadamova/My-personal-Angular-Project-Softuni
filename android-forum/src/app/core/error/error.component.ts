58ed36a75068f70fc54f2833ee38c2c91267c372
fanitaadamova <113979211+fanitaadamova@users.noreply.github.com>
2023-08-11 11:10:09 +0300

    fixed path to scss file in error component

--- android-forum/src/app/core/error/error.component.ts
+++ android-forum/src/app/core/error/error.component.ts
@@ -4,7 +4,7 @@ import { ErrorService } from './error.service';
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css'],
+  styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent implements OnInit {
   apiError$ = this.errorService.apiError$$.asObservable();
